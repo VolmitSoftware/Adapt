b39989d1bf33629955f4af98b7b363dbb21dc7b0
cyberpwn <danielmillst@gmail.com>
2021-11-21 15:58:10 -0500

    Fix superjump not working 30% of the time (events not tick)

--- src/main/java/com/volmit/adapt/content/adaptation/AgilitySuperJump.java
+++ src/main/java/com/volmit/adapt/content/adaptation/AgilitySuperJump.java
@@ -3,16 +3,28 @@ package com.volmit.adapt.content.adaptation;
 import com.volmit.adapt.api.adaptation.SimpleAdaptation;
 import com.volmit.adapt.util.C;
 import com.volmit.adapt.util.Element;
+import com.volmit.adapt.util.KMap;
+import com.volmit.adapt.util.M;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.Sound;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.event.player.PlayerVelocityEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 
 
 public class AgilitySuperJump extends SimpleAdaptation {
+    private KMap<Player, Long> lastJump = new KMap<>();
+
     public AgilitySuperJump() {
         super("super-jump");
         setDescription("Exceptional Height Advantage");
@@ -21,7 +33,7 @@ public class AgilitySuperJump extends SimpleAdaptation {
         setCostFactor(0.55);
         setMaxLevel(5);
         setInitialCost(5);
-        setInterval(50);
+        setInterval(9344);
     }
 
     private double getJumpHeight(int level) {
@@ -34,30 +46,68 @@ public class AgilitySuperJump extends SimpleAdaptation {
         v.addLore(C.YELLOW + "* 1 Complete Jump From Floor" + C.GRAY + " *kinda buggy* ");
     }
 
-    @Override
-    public void onTick() {
-        for(Player p : Bukkit.getOnlinePlayers()) {
-            if(p.isSneaking() && getLevel(p) > 0) {
-                Vector velocity = p.getVelocity();
+    @EventHandler
+    public void on(PlayerToggleSneakEvent e)
+    {
+        if(!hasAdaptation(e.getPlayer()))
+        {
+            return;
+        }
 
-                if(velocity.getY() > 0) {
-                    double jumpVelocity = 0.41999998688697815; // Default jump velocity 0.333f is the alt, not sure why the height is 0.333f as a result of this randomly...
-                    PotionEffect jumpPotion = p.getPotionEffect(PotionEffectType.JUMP);
+        if(e.isSneaking() && e.getPlayer().isOnGround()) {
+            e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.ITEM_ARMOR_EQUIP_LEATHER, 0.3f, 0.35f);
+        }
+    }
 
-                    if(jumpPotion != null) { // potion Garbage
-                        jumpVelocity += (double) ((float) jumpPotion.getAmplifier() + 1) * 0.1F;
-                    }
+    @EventHandler
+    public void on(PlayerQuitEvent e)
+    {
+        lastJump.remove(e.getPlayer());
+    }
 
-                    if(p.getLocation().getBlock().getType() != Material.LADDER && Double.compare(velocity.getY(), jumpVelocity) == 0) {
-                        p.getLocation().getWorld().playSound(p.getLocation(), Sound.BLOCK_LAVA_POP, 1.25f, 0.100f);
-                        p.setVelocity(p.getVelocity().setY(getJumpHeight(getLevel(p))));
-                    }
+    @EventHandler
+    public void on(PlayerMoveEvent e)
+    {
+        Player p = e.getPlayer();
+
+        if(p.isSwimming() || p.isFlying() || p.isGliding() || p.isSprinting()) {
+            return;
+        }
+
+        if(p.isSneaking() && getLevel(p) > 0) {
+            Vector velocity = p.getVelocity();
+
+            if(velocity.getY() > 0) {
+                double jumpVelocity = 0.4;
+                PotionEffect jumpPotion = p.getPotionEffect(PotionEffectType.JUMP);
+
+                if(jumpPotion != null) { 
+                    jumpVelocity += (double) ((float) jumpPotion.getAmplifier() + 1) * 0.1F;
+                }
+
+                if(lastJump.get(p) != null && M.ms() - lastJump.get(p) < 1000)
+                {
+                    return;
+                }
+
+                else if(lastJump.get(p) != null && M.ms() - lastJump.get(p) > 1500)
+                {
+                    lastJump.remove(p);
+                }
+
+                if(p.getLocation().getBlock().getType() != Material.LADDER && velocity.getY() > jumpVelocity && p.isOnline()) {
+                    p.getWorld().playSound(p.getLocation(), Sound.ITEM_ARMOR_EQUIP_LEATHER, 1.25f, 0.7f);
+                    p.getWorld().playSound(p.getLocation(), Sound.ITEM_ARMOR_EQUIP_LEATHER, 1.25f, 1.7f);
+                    p.getWorld().spawnParticle(Particle.BLOCK_CRACK, p.getLocation().clone().add(0, 0.3, 0), 15, 0.1, 0.8, 0.1, 0.1, p.getLocation().getBlock().getRelative(BlockFace.DOWN).getBlockData());
+                    p.setVelocity(p.getVelocity().setY(getJumpHeight(getLevel(p))));
+                    lastJump.put(p, M.ms());
                 }
             }
-            // the rest
-            if(p.isSwimming() || p.isFlying() || p.isGliding() || p.isSprinting()) {
-                return;
-            }
         }
+    }
+
+    @Override
+    public void onTick() {
+
     }
 }
