c43ea78413d3ec8a08e46fc23fa46ff402e74d5a
cyberpwn <danielmillst@gmail.com>
2021-11-26 22:02:40 -0500

    Excavation Skill

---
+++ src/main/java/com/volmit/adapt/content/skill/SkillExcavation.java
@@ -0,0 +1,74 @@
+package com.volmit.adapt.content.skill;
+
+import com.volmit.adapt.api.skill.SimpleSkill;
+import com.volmit.adapt.api.world.AdaptPlayer;
+import com.volmit.adapt.util.C;
+import com.volmit.adapt.util.J;
+import lombok.NoArgsConstructor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class SkillExcavation extends SimpleSkill<SkillExcavation.Config> {
+    public SkillExcavation() {
+        super("excavation", "\u2725");
+        registerConfiguration(Config.class);
+        setColor(C.YELLOW);
+        setInterval(5251);
+        setIcon(Material.DIAMOND_SHOVEL);
+    }
+
+    @EventHandler
+    public void on(EntityDamageByEntityEvent e) {
+        if(e.getDamager() instanceof Player p) {
+            AdaptPlayer a = getPlayer((Player) e.getDamager());
+            ItemStack hand = a.getPlayer().getInventory().getItemInMainHand();
+
+            if(isShovel(hand)) {
+                getPlayer(p).getData().addStat("excavation.swings", 1);
+                getPlayer(p).getData().addStat("excavation.damage", e.getDamage());
+                xp(a.getPlayer(), e.getEntity().getLocation(), getConfig().axeDamageXPMultiplier * e.getDamage());
+            }
+        }
+    }
+
+    @EventHandler
+    public void on(BlockBreakEvent e) {
+        if(isAxe(e.getPlayer().getInventory().getItemInMainHand())) {
+            double v = getValue(e.getBlock().getType());
+            getPlayer(e.getPlayer()).getData().addStat("excavation.blocks.broken", 1);
+            getPlayer(e.getPlayer()).getData().addStat("excavation.blocks.value", getValue(e.getBlock().getBlockData()));
+            J.a(() -> xp(e.getPlayer(), e.getBlock().getLocation().clone().add(0.5, 0.5, 0.5), blockXP(e.getBlock(), v)));
+        }
+    }
+
+    public double getValue(Material type) {
+        double value = super.getValue(type) * getConfig().valueXPMultiplier;
+        value += Math.min(getConfig().maxHardnessBonus, type.getHardness());
+        value += Math.min(getConfig().maxBlastResistanceBonus, type.getBlastResistance());
+        return value;
+    }
+
+
+    @Override
+    public void onTick() {
+
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return getConfig().enabled;
+    }
+
+    @NoArgsConstructor
+    protected static class Config {
+        boolean enabled = true;
+        double maxHardnessBonus = 9;
+        double maxBlastResistanceBonus = 10;
+        double valueXPMultiplier = 0.825;
+        double axeDamageXPMultiplier = 13.26;
+    }
+}
