c8b1497f5950bc01189a8263c986498598efa321
cyberpwn <danielmillst@gmail.com>
2022-06-30 23:23:05 -0400

    1.19 Obf mappings

--- src/main/java/com/volmit/adapt/nms/NMS_1_19.java
+++ src/main/java/com/volmit/adapt/nms/NMS_1_19.java
@@ -1,7 +1,7 @@
 package com.volmit.adapt.nms;
 
 import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientboundCooldownPacket;
+import net.minecraft.network.protocol.game.PacketPlayOutSetCooldown;
 import net.minecraft.world.item.Item;
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer;
@@ -12,11 +12,11 @@ public class NMS_1_19 implements NMS.Impl {
 
     @Override
     public void sendCooldown(Player p, Material m, int tick) {
-        sendPacket(p, new ClientboundCooldownPacket(notchianItem(m), tick));
+        sendPacket(p, new PacketPlayOutSetCooldown(notchianItem(m), tick));
     }
 
     private void sendPacket(Player player, Packet<?> packet) {
-        ((CraftPlayer)player).getHandle().connection.send(packet);
+        ((CraftPlayer)player).getHandle().b.a(packet);
     }
 
     private Item notchianItem(Material m) {
