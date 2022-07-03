6d8ca33a7065ce25148b6dede21895319a69b0b8
cyberpwn <danielmillst@gmail.com>
2022-07-03 00:47:20 -0400

    Ghost armor xp

--- src/main/java/com/volmit/adapt/content/adaptation/stealth/StealthGhostArmor.java
+++ src/main/java/com/volmit/adapt/content/adaptation/stealth/StealthGhostArmor.java
@@ -97,6 +97,7 @@ public class StealthGhostArmor extends SimpleAdaptation<StealthGhostArmor.Config
     {
         if(e.getEntity() instanceof Player p && hasAdaptation(p) && !e.isCancelled() && e.getDamage() > 0)
         {
+            xp(p, 2.5 * e.getDamage());
             J.s(() -> {
                 Collection<AttributeModifier> c = p.getAttribute(Attribute.GENERIC_ARMOR).getModifiers();
                 for (AttributeModifier i : new KList<>(c)) {
