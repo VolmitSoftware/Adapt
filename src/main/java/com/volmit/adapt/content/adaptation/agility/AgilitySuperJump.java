898364a434e7d0067946829f4fb0a9979b08fe5c
Brian Fopiano <mypsn1550@gmail.com>
2022-08-14 09:21:57 -0700

    That moment when you forget to add a License / Copyright
    Oops

--- src/main/java/com/volmit/adapt/content/adaptation/agility/AgilitySuperJump.java
+++ src/main/java/com/volmit/adapt/content/adaptation/agility/AgilitySuperJump.java
@@ -1,3 +1,21 @@
+/*------------------------------------------------------------------------------
+ -   Adapt is a Skill/Integration plugin  for Minecraft Bukkit Servers
+ -   Copyright (c) 2022 Arcane Arts (Volmit Software)
+ -
+ -   This program is free software: you can redistribute it and/or modify
+ -   it under the terms of the GNU General Public License as published by
+ -   the Free Software Foundation, either version 3 of the License, or
+ -   (at your option) any later version.
+ -
+ -   This program is distributed in the hope that it will be useful,
+ -   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ -   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ -   GNU General Public License for more details.
+ -
+ -   You should have received a copy of the GNU General Public License
+ -   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ -----------------------------------------------------------------------------*/
+
 package com.volmit.adapt.content.adaptation.agility;
 
 import com.volmit.adapt.Adapt;
@@ -46,8 +64,8 @@ public class AgilitySuperJump extends SimpleAdaptation<AgilitySuperJump.Config> 
 
     @Override
     public void addStats(int level, Element v) {
-        v.addLore(C.GREEN + "+ " + Form.pc(getJumpHeight(level), 0) + C.GRAY + " " +Adapt.dLocalize("Agility", "SuperJump", "Lore1"));
-        v.addLore(C.LIGHT_PURPLE +" " + Adapt.dLocalize("Agility", "SuperJump", "Lore2"));
+        v.addLore(C.GREEN + "+ " + Form.pc(getJumpHeight(level), 0) + C.GRAY + " " + Adapt.dLocalize("Agility", "SuperJump", "Lore1"));
+        v.addLore(C.LIGHT_PURPLE + " " + Adapt.dLocalize("Agility", "SuperJump", "Lore2"));
 
     }
 
