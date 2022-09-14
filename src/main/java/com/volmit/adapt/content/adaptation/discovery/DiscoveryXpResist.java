eddf6e32122ee5b1578320ee196c3ed514c68c03
Brian Fopiano <mypsn1550@gmail.com>
2022-09-14 00:42:40 -0700

    Fixed Xp error

--- src/main/java/com/volmit/adapt/content/adaptation/discovery/DiscoveryXpResist.java
+++ src/main/java/com/volmit/adapt/content/adaptation/discovery/DiscoveryXpResist.java
@@ -71,7 +71,7 @@ public class DiscoveryXpResist extends SimpleAdaptation<DiscoveryXpResist.Config
 
     @EventHandler
     public void on(EntityDamageEvent e) {
-        if (e.getEntity() instanceof Player p && hasAdaptation(p)) {
+        if (e.getEntity() instanceof Player p && hasAdaptation(p) && p.getLevel() > 1) {
             if (p.getLevel() < p.getLevel() - getXpTaken(getLevel(p))) {
                 vfxFastRing(p.getLocation().add(0, 0.05, 0), 1, Color.RED);
                 p.playSound(p.getLocation(), Sound.BLOCK_FUNGUS_BREAK, 15, 0.01f);
@@ -81,11 +81,15 @@ public class DiscoveryXpResist extends SimpleAdaptation<DiscoveryXpResist.Config
                 e.setDamage(e.getDamage() - (e.getDamage() * (getEffectiveness(getLevelPercent(getLevel(p))))));
                 xp(p, 5);
                 cooldowns.put(p, M.ms());
-
-                p.setLevel(p.getLevel() - getXpTaken(getLevel(p)));
-                vfxFastRing(p.getLocation().add(0, 0.05, 0), 1, Color.LIME);
-                p.playSound(p.getLocation(), Sound.ENTITY_IRON_GOLEM_REPAIR, 3, 0.01f);
-                p.playSound(p.getLocation(), Sound.BLOCK_SHROOMLIGHT_HIT, 15, 0.01f);
+                if (p.getLevel() - getXpTaken(getLevel(p)) > 0) {
+                    p.setLevel(p.getLevel() - getXpTaken(getLevel(p)));
+                    vfxFastRing(p.getLocation().add(0, 0.05, 0), 1, Color.LIME);
+                    p.playSound(p.getLocation(), Sound.ENTITY_IRON_GOLEM_REPAIR, 3, 0.01f);
+                    p.playSound(p.getLocation(), Sound.BLOCK_SHROOMLIGHT_HIT, 15, 0.01f);
+                } else {
+                    vfxFastRing(p.getLocation().add(0, 0.05, 0), 1, Color.RED);
+                    p.playSound(p.getLocation(), Sound.BLOCK_FUNGUS_BREAK, 15, 0.01f);
+                }
             } else {
                 vfxFastRing(p.getLocation().add(0, 0.05, 0), 1, Color.RED);
                 p.playSound(p.getLocation(), Sound.BLOCK_FUNGUS_BREAK, 15, 0.01f);
