ba40a6bd3469157a993be751a1c08d9cca518b5f
Brian Fopiano <mypsn1550@gmail.com>
2022-09-15 15:39:26 -0700

    New Blocking Skill/Adaptation

---
+++ src/main/java/com/volmit/adapt/content/skill/SkillBlocking.java
@@ -0,0 +1,138 @@
+/*------------------------------------------------------------------------------
+ -   Adapt is a Skill/Integration plugin  for Minecraft Bukkit Servers
+ -   Copyright (c) 2022 Arcane Arts (Volmit Software)
+ -
+ -   This program is free software: you can redistribute it and/or modify
+ -   it under the terms of the GNU General Public License as published by
+ -   the Free Software Foundation, either version 3 of the License, or
+ -   (at your option) any later version.
+ -
+ -   This program is distributed in the hope that it will be useful,
+ -   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ -   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ -   GNU General Public License for more details.
+ -
+ -   You should have received a copy of the GNU General Public License
+ -   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ -----------------------------------------------------------------------------*/
+
+package com.volmit.adapt.content.skill;
+
+import com.volmit.adapt.Adapt;
+import com.volmit.adapt.AdaptConfig;
+import com.volmit.adapt.api.skill.SimpleSkill;
+import com.volmit.adapt.api.world.AdaptPlayer;
+import com.volmit.adapt.content.adaptation.axe.AxeChop;
+import com.volmit.adapt.content.adaptation.axe.AxeDropToInventory;
+import com.volmit.adapt.content.adaptation.axe.AxeGroundSmash;
+import com.volmit.adapt.content.adaptation.axe.AxeLeafVeinminer;
+import com.volmit.adapt.util.C;
+import com.volmit.adapt.util.J;
+import lombok.NoArgsConstructor;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class SkillAxes extends SimpleSkill<SkillAxes.Config> {
+    public SkillAxes() {
+        super("axes", Adapt.dLocalize("Skill", "Axes", "Icon"));
+        registerConfiguration(Config.class);
+        setColor(C.YELLOW);
+        setDescription(Adapt.dLocalize("Skill", "Axes", "Description1") + C.ITALIC + Adapt.dLocalize("Skill", "Axes", "Description2") + C.GRAY + " " + Adapt.dLocalize("Skill", "Axes", "Description3"));
+        setDisplayName(Adapt.dLocalize("Skill", "Axes", "Name"));
+        setInterval(5251);
+        setIcon(Material.GOLDEN_AXE);
+        registerAdaptation(new AxeGroundSmash());
+        registerAdaptation(new AxeChop());
+        registerAdaptation(new AxeDropToInventory());
+        registerAdaptation(new AxeLeafVeinminer());
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void on(EntityDamageByEntityEvent e) {
+        if (!e.isCancelled()) {
+            if (e.getDamager() instanceof Player p) {
+                if (e.isCancelled()) {
+                    return;
+                }
+                if (AdaptConfig.get().blacklistedWorlds.contains(p.getWorld().getName())) {
+                    return;
+                }
+                if (!AdaptConfig.get().isXpInCreative() && (p.getGameMode().equals(GameMode.CREATIVE) || p.getGameMode().equals(GameMode.SPECTATOR))) {
+                    return;
+                }
+                AdaptPlayer a = getPlayer((Player) e.getDamager());
+                ItemStack hand = a.getPlayer().getInventory().getItemInMainHand();
+
+                if (isAxe(hand)) {
+                    getPlayer(p).getData().addStat("axes.swings", 1);
+                    getPlayer(p).getData().addStat("axes.damage", e.getDamage());
+                    xp(a.getPlayer(), e.getEntity().getLocation(), getConfig().axeDamageXPMultiplier * e.getDamage());
+                }
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void on(BlockBreakEvent e) {
+        if (e.isCancelled()) {
+            return;
+        }
+        Player p = e.getPlayer();
+        if (AdaptConfig.get().blacklistedWorlds.contains(p.getWorld().getName())) {
+            return;
+        }
+
+        if (!AdaptConfig.get().isXpInCreative() && (p.getGameMode().equals(GameMode.CREATIVE) || p.getGameMode().equals(GameMode.SPECTATOR))) {
+            return;
+        }
+        if (isAxe(p.getInventory().getItemInMainHand())) {
+            double v = getValue(e.getBlock().getType());
+            getPlayer(p).getData().addStat("axes.blocks.broken", 1);
+            getPlayer(p).getData().addStat("axes.blocks.value", getValue(e.getBlock().getBlockData()));
+            J.a(() -> xp(p, e.getBlock().getLocation().clone().add(0.5, 0.5, 0.5), blockXP(e.getBlock(), v)));
+        }
+    }
+
+    public double getValue(Material type) {
+        double value = super.getValue(type) * getConfig().valueXPMultiplier;
+        value += Math.min(getConfig().maxHardnessBonus, type.getHardness());
+        value += Math.min(getConfig().maxBlastResistanceBonus, type.getBlastResistance());
+
+        if (type.name().endsWith("_LOG") || type.name().endsWith("_WOOD")) {
+            value += getConfig().logOrWoodXPMultiplier;
+        }
+        if (type.name().endsWith("_LEAVES")) {
+            value += getConfig().leavesMultiplier;
+        }
+
+        return value;
+    }
+
+
+    @Override
+    public void onTick() {
+
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return getConfig().enabled;
+    }
+
+    @NoArgsConstructor
+    protected static class Config {
+        boolean enabled = true;
+        double maxHardnessBonus = 9;
+        double maxBlastResistanceBonus = 10;
+        double logOrWoodXPMultiplier = 9.67;
+        double leavesMultiplier = 3.11;
+        double valueXPMultiplier = 0.225;
+        double axeDamageXPMultiplier = 13.26;
+    }
+}
