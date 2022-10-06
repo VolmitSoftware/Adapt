aaaf1c3d006d5cbeca82c08d8f31f66a70e13334
Brian Fopiano <mypsn1550@gmail.com>
2022-10-05 21:16:09 -0700

    how did this compile before?

--- src/main/java/com/volmit/adapt/util/UIElement.java
+++ src/main/java/com/volmit/adapt/util/UIElement.java
@@ -20,6 +20,7 @@ package com.volmit.adapt.util;
 
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
