83ca89188a5b19132f705dfd7ca47841f46fceb2
Brian Neumann-Fopiano <mypsn1550@gmail.com>
2022-10-11 15:36:54 -0700

    Fix: #263

--- src/main/java/com/volmit/adapt/api/Component.java
+++ src/main/java/com/volmit/adapt/api/Component.java
@@ -18,6 +18,8 @@
 
 package com.volmit.adapt.api;
 
+import com.francobm.magicosmetics.api.CosmeticType;
+import com.francobm.magicosmetics.api.MagicAPI;
 import com.google.common.collect.Lists;
 import com.volmit.adapt.AdaptConfig;
 import com.volmit.adapt.api.data.WorldData;
@@ -104,6 +106,8 @@ public interface Component {
         ItemStack pants = inv.getLeggings();
         double armorValue = 0.0;
         if (helmet == null) armorValue = armorValue + 0.0;
+
+        else if (MagicAPI.hasEquipCosmetic(player, CosmeticType.HAT)) armorValue = armorValue + 0;
         else if (helmet.getType() == Material.LEATHER_HELMET) armorValue = armorValue + 0.04;
         else if (helmet.getType() == Material.GOLDEN_HELMET) armorValue = armorValue + 0.08;
         else if (helmet.getType() == Material.TURTLE_HELMET) armorValue = armorValue + 0.08;
@@ -129,6 +133,7 @@ public interface Component {
         else if (pants.getType() == Material.NETHERITE_LEGGINGS) armorValue = armorValue + 0.24;
         //
         if (chest == null) armorValue = armorValue + 0.0;
+        else if (MagicAPI.hasEquipCosmetic(player, CosmeticType.BAG)) armorValue = armorValue + 0;
         else if (chest.getType() == Material.LEATHER_CHESTPLATE) armorValue = armorValue + 0.12;
         else if (chest.getType() == Material.GOLDEN_CHESTPLATE) armorValue = armorValue + 0.20;
         else if (chest.getType() == Material.CHAINMAIL_CHESTPLATE) armorValue = armorValue + 0.20;
