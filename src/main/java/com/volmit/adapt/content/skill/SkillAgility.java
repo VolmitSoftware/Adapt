6959be846b0f42e4f985293b2b11779e0f4f1387
Brian Neumann-Fopiano <mypsn1550@gmail.com>
2023-04-25 16:10:02 -0400

    more twekas before versioning

--- src/main/java/com/volmit/adapt/content/skill/SkillAgility.java
+++ src/main/java/com/volmit/adapt/content/skill/SkillAgility.java
@@ -33,13 +33,20 @@ import com.volmit.adapt.util.advancements.advancement.AdvancementVisibility;
 import lombok.NoArgsConstructor;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class SkillAgility extends SimpleSkill<SkillAgility.Config> {
+    private Map<UUID, Location> lastLocations;
+
     public SkillAgility() {
         super("agility", Localizer.dLocalize("skill", "agility", "icon"));
         registerConfiguration(Config.class);
@@ -95,6 +102,7 @@ public class SkillAgility extends SimpleSkill<SkillAgility.Config> {
         registerStatTracker(AdaptStatTracker.builder().advancement("challenge_sprint_50k").goal(50000).stat("move").reward(getConfig().challengeSprint5kReward).build());
         registerStatTracker(AdaptStatTracker.builder().advancement("challenge_sprint_500k").goal(500000).stat("move").reward(getConfig().challengeSprint5kReward).build());
         registerStatTracker(AdaptStatTracker.builder().advancement("challenge_sprint_marathon").goal(42195).stat("move").reward(getConfig().challengeSprintMarathonReward).build());
+        lastLocations = new HashMap<>();
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
@@ -124,6 +132,7 @@ public class SkillAgility extends SimpleSkill<SkillAgility.Config> {
         }
     }
 
+
     @Override
     public void onTick() {
         if (!this.isEnabled()) {
@@ -134,11 +143,40 @@ public class SkillAgility extends SimpleSkill<SkillAgility.Config> {
             if (AdaptConfig.get().blacklistedWorlds.contains(i.getWorld().getName())) {
                 return;
             }
-            if (i.isSprinting() && !i.isFlying() && !i.isSwimming() && !i.isSneaking()) {
-                if (!AdaptConfig.get().isXpInCreative() && (i.getGameMode().equals(GameMode.CREATIVE) || i.getGameMode().equals(GameMode.SPECTATOR))) {
-                    return;
+            if (!AdaptConfig.get().isXpInCreative() && (i.getGameMode().equals(GameMode.CREATIVE) || i.getGameMode().equals(GameMode.SPECTATOR))) {
+                return;
+            }
+
+            // Check for distance moved
+            UUID playerId = i.getUniqueId();
+            Location currentLocation = i.getLocation();
+            if (lastLocations.containsKey(playerId)) {
+                Location lastLocation = lastLocations.get(playerId);
+                if (lastLocation.getWorld().equals(currentLocation.getWorld())) {
+                    double distanceMoved = lastLocation.distance(currentLocation);
+                    xpSilent(i, getConfig().moveXpPassive * distanceMoved);
                 }
+            }
+            lastLocations.put(playerId, currentLocation);
+
+            // Check for sprinting
+            if (i.isSprinting() && !i.isFlying() && !i.isSwimming() && !i.isSneaking()) {
                 xpSilent(i, getConfig().sprintXpPassive);
+            }
+
+            // Check for swimming
+            if (i.isSwimming() && !i.isFlying() && !i.isSprinting() && !i.isSneaking()) {
+                xpSilent(i, getConfig().swimXpPassive);
+            }
+
+            // Check for jumping
+            if (i.getLocation().subtract(0, 1, 0).getBlock().getType().isAir() && !i.isFlying() && !i.isSneaking()) {
+                xpSilent(i, getConfig().jumpXpPassive);
+            }
+
+            // Check for climbing ladders
+            if (i.getLocation().getBlock().getType() == Material.LADDER && !i.isFlying() && !i.isSneaking()) {
+                xpSilent(i, getConfig().climbXpPassive);
             }
         }
     }
@@ -155,5 +193,9 @@ public class SkillAgility extends SimpleSkill<SkillAgility.Config> {
         double challengeSprint5kReward = 2000;
         double challengeSprintMarathonReward = 6500;
         double sprintXpPassive = 1.25;
+        double swimXpPassive = 1.25;
+        double jumpXpPassive = 0.25;
+        double climbXpPassive = 1.25;
+        double moveXpPassive = 0.1;
     }
 }
