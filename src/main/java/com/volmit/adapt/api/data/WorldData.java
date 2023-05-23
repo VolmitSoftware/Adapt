1c42ead5b4ef6fca935a8d4d04d8f2838e9d4701
Brian Neumann-Fopiano <mypsn1550@gmail.com>
2023-05-23 06:36:58 -0400

    d

--- src/main/java/com/volmit/adapt/api/data/WorldData.java
+++ src/main/java/com/volmit/adapt/api/data/WorldData.java
@@ -48,7 +48,7 @@ public class WorldData extends TickedObject {
     public WorldData(World world) {
         super("world-data", world.getUID().toString(), 30_000);
         this.world = world;
-        mantle = new Mantle(Adapt.instance.getDataFolder("data", "mantle"), 256);
+        mantle = new Mantle(Adapt.instance.getDataFolder("data", "mantle"), world.getMaxHeight());
     }
 
     public static void stop() {
