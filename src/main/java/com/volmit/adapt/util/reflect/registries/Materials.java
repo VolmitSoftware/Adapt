3603f2eb6b1c75a922c81e6faca51b17b59adbcb
Julian Krings <juliankrings6@gmail.com>
2025-03-01 15:28:38 +0100

    fix mace not being counted as a tool

--- src/main/java/com/volmit/adapt/util/reflect/registries/Materials.java
+++ src/main/java/com/volmit/adapt/util/reflect/registries/Materials.java
@@ -1,7 +1,9 @@
 package com.volmit.adapt.util.reflect.registries;
 
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 
 public class Materials {
     public static final Material GRASS = RegistryUtil.find(Material.class, "grass", "short_grass");
+    public static final NamespacedKey MACE_KEY = NamespacedKey.minecraft("mace");
 }
