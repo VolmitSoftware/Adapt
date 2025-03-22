0724217a2b8ad34b11f6232be407fa0a2ac6f293
Julian Krings <juliankrings6@gmail.com>
2025-03-22 14:12:20 +0100

    fix spawner dropping twice with drop to inventory skill

--- src/main/java/com/volmit/adapt/content/adaptation/pickaxe/PickaxeSilkSpawner.java
+++ src/main/java/com/volmit/adapt/content/adaptation/pickaxe/PickaxeSilkSpawner.java
@@ -4,6 +4,7 @@ import com.volmit.adapt.api.adaptation.SimpleAdaptation;
 import com.volmit.adapt.util.C;
 import com.volmit.adapt.util.Element;
 import com.volmit.adapt.util.Localizer;
+import com.volmit.adapt.util.RNG;
 import com.volmit.adapt.util.collection.KList;
 import lombok.NoArgsConstructor;
 import org.bukkit.Bukkit;
@@ -18,6 +19,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BlockStateMeta;
 
 public class PickaxeSilkSpawner extends SimpleAdaptation<PickaxeSilkSpawner.Config> {
+    private final RNG rng = new RNG();
 
     public PickaxeSilkSpawner() {
         super("pickaxe-silk-spawner");
@@ -54,13 +56,25 @@ public class PickaxeSilkSpawner extends SimpleAdaptation<PickaxeSilkSpawner.Conf
             spawner.setItemMeta(meta);
         }
 
-        block.getWorld().dropItemNaturally(block.getLocation(), spawner, items::add);
+        var loc = block.getLocation().add(
+                rng.d(-0.25D, 0.25D),
+                rng.d(-0.25D, 0.25D) - 0.125D,
+                rng.d(-0.25D, 0.25D)
+        );
+        var item = block.getWorld().createEntity(loc, Item.class);
+        item.setItemStack(spawner);
+        item.setOwner(player.getUniqueId());
 
         var dropEvent = new BlockDropItemEvent(block, state, player, items);
         Bukkit.getPluginManager().callEvent(dropEvent);
-        if (dropEvent.isCancelled() && !items.isEmpty()) {
-            items.forEach(Item::remove);
-            items.clear();
+        if (dropEvent.isCancelled()) {
+            for (Item i : dropEvent.getItems()) {
+                if (i.isValid()) i.remove();
+            }
+        } else {
+            for (Item i : dropEvent.getItems()) {
+                if (!i.isValid()) block.getWorld().addEntity(i);
+            }
         }
     }
 
