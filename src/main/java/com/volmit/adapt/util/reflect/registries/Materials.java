42cec0213ff2ebd8be4ddf0e72293bb65380e343
Julian Krings <juliankrings6@gmail.com>
2025-03-31 19:54:55 +0200

    update materials and item listings

--- src/main/java/com/volmit/adapt/util/reflect/registries/Materials.java
+++ src/main/java/com/volmit/adapt/util/reflect/registries/Materials.java
@@ -1,9 +1,92 @@
 package com.volmit.adapt.util.reflect.registries;
 
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 
 public class Materials {
     public static final Material GRASS = RegistryUtil.find(Material.class, "grass", "short_grass");
-    public static final NamespacedKey MACE_KEY = NamespacedKey.minecraft("mace");
+
+    /* Added Materials */
+    public static final Material MACE = RegistryUtil.findNullable(Material.class, "mace");
+    //1.20+
+    public static final Material OAK_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "oak_hanging_sign");
+    public static final Material OAK_WALL_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "oak_wall_hanging_sign");
+    public static final Material SPRUCE_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "spruce_hanging_sign");
+    public static final Material SPRUCE_WALL_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "spruce_wall_hanging_sign");
+    public static final Material BIRCH_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "birch_hanging_sign");
+    public static final Material BIRCH_WALL_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "birch_wall_hanging_sign");
+    public static final Material JUNGLE_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "jungle_hanging_sign");
+    public static final Material JUNGLE_WALL_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "jungle_wall_hanging_sign");
+    public static final Material ACACIA_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "acacia_hanging_sign");
+    public static final Material ACACIA_WALL_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "acacia_wall_hanging_sign");
+    public static final Material DARK_OAK_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "dark_oak_hanging_sign");
+    public static final Material DARK_OAK_WALL_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "dark_oak_wall_hanging_sign");
+    public static final Material MANGROVE_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "mangrove_hanging_sign");
+    public static final Material MANGROVE_WALL_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "mangrove_wall_hanging_sign");
+    public static final Material CRIMSON_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "crimson_hanging_sign");
+    public static final Material CRIMSON_WALL_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "crimson_wall_hanging_sign");
+    public static final Material WARPED_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "warped_hanging_sign");
+    public static final Material WARPED_WALL_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "warped_wall_hanging_sign");
+
+    public static final Material CHERRY_LOG = RegistryUtil.findNullable(Material.class, "cherry_log");
+    public static final Material CHERRY_WOOD = RegistryUtil.findNullable(Material.class, "cherry_wood");
+    public static final Material STRIPPED_CHERRY_LOG = RegistryUtil.findNullable(Material.class, "stripped_cherry_log");
+    public static final Material STRIPPED_CHERRY_WOOD = RegistryUtil.findNullable(Material.class, "stripped_cherry_wood");
+    public static final Material CHERRY_PLANKS = RegistryUtil.findNullable(Material.class, "cherry_planks");
+    public static final Material CHERRY_STAIRS = RegistryUtil.findNullable(Material.class, "cherry_stairs");
+    public static final Material CHERRY_SLAB = RegistryUtil.findNullable(Material.class, "cherry_slab");
+    public static final Material CHERRY_FENCE = RegistryUtil.findNullable(Material.class, "cherry_fence");
+    public static final Material CHERRY_FENCE_GATE = RegistryUtil.findNullable(Material.class, "cherry_fence_gate");
+    public static final Material CHERRY_DOOR = RegistryUtil.findNullable(Material.class, "cherry_door");
+    public static final Material CHERRY_TRAPDOOR = RegistryUtil.findNullable(Material.class, "cherry_trapdoor");
+    public static final Material CHERRY_PRESSURE_PLATE = RegistryUtil.findNullable(Material.class, "cherry_pressure_plate");
+    public static final Material CHERRY_BUTTON = RegistryUtil.findNullable(Material.class, "cherry_button");
+    public static final Material CHERRY_SIGN = RegistryUtil.findNullable(Material.class, "cherry_sign");
+    public static final Material CHERRY_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "cherry_hanging_sign");
+    public static final Material CHERRY_WALL_SIGN = RegistryUtil.findNullable(Material.class, "cherry_wall_sign");
+    public static final Material CHERRY_WALL_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "cherry_wall_hanging_sign");
+    public static final Material CHERRY_LEAVES = RegistryUtil.findNullable(Material.class, "cherry_leaves");
+    public static final Material CHERRY_SAPLING = RegistryUtil.findNullable(Material.class, "cherry_sapling");
+
+    public static final Material BAMBOO_BLOCK = RegistryUtil.findNullable(Material.class, "bamboo_block");
+    public static final Material STRIPPED_BAMBOO_BLOCK = RegistryUtil.findNullable(Material.class, "stripped_bamboo_block");
+    public static final Material BAMBOO_PLANKS = RegistryUtil.findNullable(Material.class, "bamboo_planks");
+    public static final Material BAMBOO_MOSAIC = RegistryUtil.findNullable(Material.class, "bamboo_mosaic");
+    public static final Material BAMBOO_STAIRS = RegistryUtil.findNullable(Material.class, "bamboo_stairs");
+    public static final Material BAMBOO_MOSAIC_STAIRS = RegistryUtil.findNullable(Material.class, "bamboo_mosaic_stairs");
+    public static final Material BAMBOO_SLAB = RegistryUtil.findNullable(Material.class, "bamboo_slab");
+    public static final Material BAMBOO_MOSAIC_SLAB = RegistryUtil.findNullable(Material.class, "bamboo_mosaic_slab");
+    public static final Material BAMBOO_FENCE = RegistryUtil.findNullable(Material.class, "bamboo_fence");
+    public static final Material BAMBOO_FENCE_GATE = RegistryUtil.findNullable(Material.class, "bamboo_fence_gate");
+    public static final Material BAMBOO_DOOR = RegistryUtil.findNullable(Material.class, "bamboo_door");
+    public static final Material BAMBOO_TRAPDOOR = RegistryUtil.findNullable(Material.class, "bamboo_trapdoor");
+    public static final Material BAMBOO_PRESSURE_PLATE = RegistryUtil.findNullable(Material.class, "bamboo_pressure_plate");
+    public static final Material BAMBOO_BUTTON = RegistryUtil.findNullable(Material.class, "bamboo_button");
+    public static final Material BAMBOO_SIGN = RegistryUtil.findNullable(Material.class, "bamboo_sign");
+    public static final Material BAMBOO_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "bamboo_hanging_sign");
+    public static final Material BAMBOO_WALL_SIGN = RegistryUtil.findNullable(Material.class, "bamboo_wall_sign");
+    public static final Material BAMBOO_WALL_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "bamboo_wall_hanging_sign");
+
+    //1.21.4+
+    public static final Material PALE_OAK_LOG = RegistryUtil.findNullable(Material.class, "pale_oak_log");
+    public static final Material PALE_OAK_WOOD = RegistryUtil.findNullable(Material.class, "pale_oak_wood");
+    public static final Material STRIPPED_PALE_OAK_LOG = RegistryUtil.findNullable(Material.class, "stripped_pale_oak_log");
+    public static final Material STRIPPED_PALE_OAK_WOOD = RegistryUtil.findNullable(Material.class, "stripped_pale_oak_wood");
+
+    public static final Material PALE_OAK_PLANKS = RegistryUtil.findNullable(Material.class, "pale_oak_planks");
+    public static final Material PALE_OAK_STAIRS = RegistryUtil.findNullable(Material.class, "pale_oak_stairs");
+    public static final Material PALE_OAK_SLAB = RegistryUtil.findNullable(Material.class, "pale_oak_slab");
+    public static final Material PALE_OAK_FENCE = RegistryUtil.findNullable(Material.class, "pale_oak_fence");
+    public static final Material PALE_OAK_FENCE_GATE = RegistryUtil.findNullable(Material.class, "pale_oak_fence_gate");
+    public static final Material PALE_OAK_DOOR = RegistryUtil.findNullable(Material.class, "pale_oak_door");
+    public static final Material PALE_OAK_TRAPDOOR = RegistryUtil.findNullable(Material.class, "pale_oak_trapdoor");
+    public static final Material PALE_OAK_PRESSURE_PLATE = RegistryUtil.findNullable(Material.class, "pale_oak_pressure_plate");
+    public static final Material PALE_OAK_BUTTON = RegistryUtil.findNullable(Material.class, "pale_oak_button");
+
+    public static final Material PALE_OAK_SIGN = RegistryUtil.findNullable(Material.class, "pale_oak_sign");
+    public static final Material PALE_OAK_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "pale_oak_hanging_sign");
+    public static final Material PALE_OAK_WALL_SIGN = RegistryUtil.findNullable(Material.class, "pale_oak_wall_sign");
+    public static final Material PALE_OAK_WALL_HANGING_SIGN = RegistryUtil.findNullable(Material.class, "pale_oak_wall_hanging_sign");
+
+    public static final Material PALE_OAK_LEAVES = RegistryUtil.findNullable(Material.class, "pale_oak_leaves");
+    public static final Material PALE_OAK_SAPLING = RegistryUtil.findNullable(Material.class, "pale_oak_sapling");
 }
